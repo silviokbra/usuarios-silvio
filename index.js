d6c29ccd9ba568bb08656c46b9027697ea1281df
Silvio Seferin <silvioseferin@gmail.com>
2025-01-22 12:59:20 -0300

    Criado um evento listener para o botão submit do formulário, e colocado o json dentro dele

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
-fields.forEach(function (field, index) {
 
-    if (field.name == "gender") {
+document.getElementById("form-user-create").addEventListener("submit", function (event) {
 
-        if (field.checked) {
+    event.preventDefault();
+
+    fields.forEach(function (field, index) {
+
+        if (field.name == "gender") {
+
+            if (field.checked) {
+                user[field.name] = field.value;
+            }
+
+        } else {
+
             user[field.name] = field.value;
         }
 
-    } else {
+    });
+    console.log(user);
 
-        user[field.name] = field.value;
-    }
-
-});
-console.log(user);
+})
