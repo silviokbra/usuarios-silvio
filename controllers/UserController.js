b00dc74d7bed02cdb1d6f3fa8e46c7dc91dab5d2
Silvio Seferin <silvioseferin@gmail.com>
2025-01-27 17:18:20 -0300

    Fazendo o botão excluir, salvar no localStorage, e refactoring no código

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -173,21 +173,9 @@ class UserController {
 
     }
 
-
-    getUsersStorage() {
-
-        let users = [];
-
-        if (localStorage.getItem("users")) {
-
-            users = JSON.parse(localStorage.getItem("users"));
-        }
-        return users;
-    }
-
     selectAll() {
 
-        let users = this.getUsersStorage();
+        let users = User.getUsersStorage();
 
         users.forEach(dataUser => {
 
@@ -238,6 +226,11 @@ class UserController {
         tr.querySelector(".btn-delete").addEventListener("click", e => {
 
             if (confirm("Deseja realmente excluir?")) {
+
+                let user = new User();
+                user.loadFromJSON(JSON.parse(tr.dataset.user));
+
+                user.remove();
 
                 tr.remove();
                 this.updateCount();
