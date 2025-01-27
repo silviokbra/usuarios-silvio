18570903d863e1495b6a7df54fe79e138db61a8b
Silvio Seferin <silvioseferin@gmail.com>
2025-01-27 13:34:40 -0300

    Adicionado requireds no HTML, e função de não deixar enviar vazio no js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -70,8 +70,17 @@ class UserController {
     getValues() {
 
         let user = {};
+        let isValid = true;
 
         [...this.formEl.elements].forEach(function (field, index) {
+
+            if (["name", "email", "password"].indexOf(field.name) > -1 && !field.value) {
+
+                field.parentElement.classList.add("has-error");
+                isValid = false;
+
+            }
+
 
             if (field.name == "gender") {
 
@@ -89,6 +98,12 @@ class UserController {
             }
 
         });
+
+
+        if (!isValid) {
+            return false;
+
+        }
 
         return new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.admin, user.admin);
 
